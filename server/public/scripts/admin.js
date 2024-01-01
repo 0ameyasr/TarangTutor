e2b3c67abced8119267b291775ba59ff7a7e47c3
Ameya Srivastava <0ameya.sr@gmail.com>
2024-01-01 14:46:38 +0530

    UI/UX changes & admin

---
+++ server/public/scripts/admin.js
@@ -0,0 +1,25 @@
+
+// GET index
+document.querySelector(".header-home-icon").addEventListener("click", function () {
+  window.location.href = "/";
+});
+
+// GET index
+document.querySelector(".header-home").addEventListener("click", function () {
+  window.location.href = "/";
+});
+
+// GET portfolio
+document.querySelector(".header-portfolio").addEventListener("click", function () {
+  window.location.href = "/portfolio";
+});
+
+// GET testimonials
+document.querySelector(".header-testimonials").addEventListener("click", function () {
+  window.location.href = "/testimonials";
+});
+
+// GET courses
+document.querySelector(".header-courses").addEventListener("click", function () {
+  window.location.href = "/courses";
+});
---
+++ server/public/scripts/fetchCourses.js
@@ -0,0 +1,15 @@
+document
+  .querySelector(".neet_phy")
+  .addEventListener("click", function () {
+    window.location.href = "/courses";
+  });
+  document
+  .querySelector(".jee_phy_a")
+  .addEventListener("click", function () {
+    window.location.href = "/courses";
+  });
+  document
+  .querySelector(".jee_phy_b")
+  .addEventListener("click", function () {
+    window.location.href = "/courses";
+  });
